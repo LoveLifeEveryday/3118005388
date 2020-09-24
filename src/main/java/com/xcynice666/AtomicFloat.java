02ff5b6b44ea75594960ccac6e3b762f66180871
LoveLifeEveryday <913710642@qq.com>
2020-09-24 17:44:37 +0800

    refactor(AtomicFloat):去除了无用的代码，改善了代码质量

--- src/main/java/com/xcynice666/AtomicFloat.java
+++ src/main/java/com/xcynice666/AtomicFloat.java
@@ -1,7 +1,6 @@
 package com.xcynice666;
+
 import java.util.concurrent.atomic.AtomicInteger;
-
-
 
 
 /**
@@ -22,8 +21,13 @@ public class AtomicFloat extends Number {
         bits = new AtomicInteger(Float.floatToIntBits(initialValue));
     }
 
-    //叠加
-    public final float addAndGet(float delta) {
+
+    /**
+     * 加操作
+     *
+     * @param delta 变量
+     */
+    public final void addAndGet(float delta) {
         float expect;
         float update;
         do {
@@ -31,52 +35,13 @@ public class AtomicFloat extends Number {
             update = expect + delta;
         } while (this.compareAndSet(expect, update));
 
-        return update;
     }
 
-    public final float getAndAdd(float delta) {
-        float expect;
-        float update;
-        do {
-            expect = get();
-            update = expect + delta;
-        } while (this.compareAndSet(expect, update));
-
-        return expect;
-    }
-
-    public final float getAndDecrement() {
-        return getAndAdd(-1);
-    }
-
-    public final float decrementAndGet() {
-        return addAndGet(-1);
-    }
-
-    public final float getAndIncrement() {
-        return getAndAdd(1);
-    }
-
-    public final float incrementAndGet() {
-        return addAndGet(1);
-    }
-
-    public final float getAndSet(float newValue) {
-        float expect;
-        do {
-            expect = get();
-        } while (this.compareAndSet(expect, newValue));
-
-        return expect;
-    }
 
     public final boolean compareAndSet(float expect, float update) {
         return !bits.compareAndSet(Float.floatToIntBits(expect), Float.floatToIntBits(update));
     }
 
-    public final void set(float newValue) {
-        bits.set(Float.floatToIntBits(newValue));
-    }
 
     public final float get() {
         return Float.intBitsToFloat(bits.get());
@@ -89,7 +54,7 @@ public class AtomicFloat extends Number {
 
     @Override
     public double doubleValue() {
-        return  floatValue();
+        return floatValue();
     }
 
     @Override
